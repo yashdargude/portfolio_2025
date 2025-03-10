f828e4e4cca419fac74c281a5930a1661905eebd
yashdargude <yashdargude567@gmail.com>
2025-03-11 02:10:57 +0530

    added project

--- constants/projectCardsData.ts
+++ constants/projectCardsData.ts
@@ -2,6 +2,7 @@ import pr1 from "@/public/billify.png";
 import pr2 from "@/public/campusnotes.png";
 import pr3 from "@/public/greengridin.png";
 import pr4 from "@/public/findyourguide.png";
+import pr5 from "@/public/logstreamengine.png";
 
 import { StaticImageData } from "next/image";
 
@@ -26,15 +27,15 @@ export const projectList: projectData[] = [
     alt: "Campus Notes project",
   },
   {
-    title: "GreenGridin",
-    link: "https://greengridin.vercel.app/",
-    image: pr3,
-    alt: "GreenGridin project",
-  },
-  {
     title: "Log-Stream Engine",
     link: "https://log-stream-engine.vercel.app/",
     image: pr5,
     alt: "Log Stream Engine project",
+  },
+  {
+    title: "GreenGridin",
+    link: "https://greengridin.vercel.app/",
+    image: pr3,
+    alt: "GreenGridin project",
   },
 ];
